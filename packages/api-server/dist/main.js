aa63ddc69262a97858a0d1f72f57f8a2a199dc5f
CheolJe <me@falsy.me>
2025-02-18 00:49:22 +0900

    chore(server): 프로그램 검수를 위한 임시 CORS 허용 (빌드 코드 추가)

--- packages/api-server/dist/main.js
+++ packages/api-server/dist/main.js
@@ -7,19 +7,8 @@ async function bootstrap() {
     const app = await core_1.NestFactory.create(AppModule_1.AppModule);
     const isDev = process.env.NODE_ENV === "development";
     const port = isDev ? 3000 : process.env.PORT;
-    const allowedOrigins = [
-        `chrome-extension://${process.env.EXTENSION_ID}`,
-        `${process.env.DEV_CLIENT_URL}`
-    ];
     app.enableCors({
-        origin: (origin, callback) => {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            }
-            else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
+        origin: "*",
         credentials: true,
         exposedHeaders: ["ETag"]
     });
