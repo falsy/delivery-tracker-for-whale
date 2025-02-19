839303e77bdcaa9a1dff091dd1f4bfef9db89330
CheolJe <me@falsy.me>
2025-02-20 02:44:55 +0900

    chore(server): CORS 제한 설정 (검수 완료)

--- packages/api-server/src/main.ts
+++ packages/api-server/src/main.ts
@@ -7,20 +7,20 @@ async function bootstrap() {
   const isDev = process.env.NODE_ENV === "development"
   const port = isDev ? 3000 : process.env.PORT
 
-  // const allowedOrigins = [
-  //   `chrome-extension://${process.env.EXTENSION_ID}`,
-  //   `${process.env.DEV_CLIENT_URL}`
-  // ]
+  const allowedOrigins = [
+    `chrome-extension://${process.env.EXTENSION_ID}`,
+    `${process.env.DEV_CLIENT_URL}`
+  ]
 
   app.enableCors({
-    // origin: (origin, callback) => {
-    //   if (!origin || allowedOrigins.includes(origin)) {
-    //     callback(null, true)
-    //   } else {
-    //     callback(new Error("Not allowed by CORS"))
-    //   }
-    // },
-    origin: "*",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error("Not allowed by CORS"))
+      }
+    },
+    // origin: "*",
     credentials: true,
     exposedHeaders: ["ETag"]
   })
