4086af33fb664b965d5d66cdb62d7ca5d9a2f905
CheolJe <me@falsy.me>
2025-03-08 11:12:20 +0900

    feat: NX 추가 및 설정

--- packages/client/src/constants/index.ts
+++ packages/client/src/constants/index.ts
@@ -1,3 +1,3 @@
-export const API_URL = "https://parcel.falsy.me"
+export const API_URL = import.meta.env.VITE_API_URL
 export const TRACKER_LIST = "TRACKER_LIST"
 export const CARRIER_LIST = "CARRIER_LIST"
