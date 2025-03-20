327f780991417dc9700f386bf79fafedee9d8efb
CheolJe <me@falsy.me>
2025-03-21 01:18:58 +0900

    feat: 모로레포 구성 제거, 클라이언트 코드만 유지

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,25 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 2022,
-    sourceType: "module"
-  },
-  plugins: ["@typescript-eslint/eslint-plugin"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended"
-  ],
   root: true,
   env: {
     es6: true,
     browser: true,
     node: true
   },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module"
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended"
+  ],
+  plugins: [],
   rules: {
+    "react/react-in-jsx-scope": "off",
     "@typescript-eslint/no-require-imports": "off"
   }
 }
