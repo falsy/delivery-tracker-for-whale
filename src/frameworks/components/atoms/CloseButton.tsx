0179fcb837e88fb66fc1190812fb87ea679a8b80
CheolJe <me@falsy.me>
2025-05-05 03:11:10 +0900

    feat: 세부 스타일 수정

--- src/frameworks/components/atoms/CloseButton.tsx
+++ src/frameworks/components/atoms/CloseButton.tsx
@@ -1,7 +1,7 @@
 export default function CloseButton({ onClick }: { onClick: () => void }) {
   return (
     <button
-      className="block w-full mt-5 text-sm text-center leading-[36px] p-0 bg-buttonBG border border-border dark:bg-buttonBG-dark dark:border-border-dark text-black dark:text-white cursor-pointer"
+      className="block w-full mt-2 text-sm text-center leading-[36px] p-0 bg-buttonBG border border-border dark:bg-buttonBG-dark dark:border-border-dark text-black dark:text-white cursor-pointer"
       onClick={onClick}
     >
       닫기
